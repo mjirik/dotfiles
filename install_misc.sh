3f543ad8fe322659f0f4dc0e2e9f6bcb7b8068f1
M. Jirik <miroslav.jirik@gmail.com>
2015-11-02 09:08:34 +0100

    copy ssh to pole and office-desktop-pc

--- install_misc.sh
+++ install_misc.sh
@@ -14,7 +14,10 @@ scp mjirik@147.228.240.61:/home/mjirik/ssh/id_rsa ~/.ssh/id_rsa
 scp mjirik@147.228.47.162:/home/mjirik/.pypirc ~/.pypirc
 # nebo radeji vygenerovat ?
 # ssh-keygen
-# ssh-copy-id ~/.ssh/id_rsa.pub mjirik@147.228.47.162
+# pole
+# ssh-copy-id mjirik@147.228.240.61
+# skolni pocitac 
+# ssh-copy-id mjirik@147.228.47.162
 
 # this repository setup
 git remote set-url origin git@github.com:mjirik/dotfiles.git
