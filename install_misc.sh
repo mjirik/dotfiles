f4885b85596a37c3b4574f8425e59c26d11e2e0d
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-05-21 09:00:53 +0200

    autofs un533

--- install_misc.sh
+++ install_misc.sh
@@ -76,9 +76,12 @@ sudo touch /etc/auto.master
 filetext="synologypublic"
 if ! grep -q "$filetext" "/etc/auto.sshfs"; then
     echo "set /etc/auto.sshfs"
-    sudo sh -c 'echo "synologypublic -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536,IdentityFile=/home/mjirik/.ssh/id_rsa,UserKnownHostsFile=/home/mjirik/.ssh/known_hosts :sshfs\#mjirik@192.168.1.95:/public" >> /etc/auto.sshfs'
-    sudo sh -c 'echo "polepublic -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536,IdentityFile=/home/mjirik/.ssh/id_rsa,UserKnownHostsFile=/home/mjirik/.ssh/known_hosts :sshfs\#mjirik@147.228.240.61:/public" >> /etc/auto.sshfs'
+    # sudo sh -c 'echo "synologypublic -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536,IdentityFile=/home/mjirik/.ssh/id_rsa,UserKnownHostsFile=/home/mjirik/.ssh/known_hosts :sshfs\#mjirik@192.168.1.95:/public" >> /etc/auto.sshfs'
+    # sudo sh -c 'echo "polepublic -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536,IdentityFile=/home/mjirik/.ssh/id_rsa,UserKnownHostsFile=/home/mjirik/.ssh/known_hosts :sshfs\#mjirik@147.228.240.61:/public" >> /etc/auto.sshfs'
+    sudo cat $actualdir/auto.fs.append >> /etc/auto.sshfs
     sudo sh -c 'echo "/home/mjirik/autofs /etc/auto.sshfs uid=1000,gid=1000,--timeout=30,--ghost" >> /etc/auto.master'
+    sudo /etc/init.d/autofs reload
+    # sudo /etc/init.d/autofs restart
 fi
 
 
