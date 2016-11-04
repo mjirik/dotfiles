755f255de05b67989c8ff469e6fb829ce88ae4d9
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-11-04 09:24:30 +0100

    remove --delete

--- bin/mysync.sh
+++ bin/mysync.sh
@@ -6,6 +6,7 @@
 # Distributed under terms of the MIT license.
 #
 
-rsync -ave ssh --delete ~/projects/paper_graphics/ mjirik@147.228.240.61:/mnt/proNAS/public/queetech/paper_graphics/
+rsync -ave ssh ~/projects/paper_graphics/ mjirik@147.228.240.61:/mnt/proNAS/public/queetech/paper_graphics/
+# rsync -ave ssh --delete ~/projects/paper_graphics/ mjirik@147.228.240.61:/mnt/proNAS/public/queetech/paper_graphics/
 
 
