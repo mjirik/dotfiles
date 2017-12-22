f11b0619fda5bc1ddededb1d73f74f59ff3eb912
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-12-22 03:45:16 +0100

    fix path

--- bin/mysync.sh
+++ bin/mysync.sh
@@ -7,7 +7,7 @@
 #
 
 rsync -O -ave ssh ~/projects/paper_graphics/ mjirik@147.228.240.61:/mnt/proNAS/korpusy/queetech/paper_graphics/
-rsync -O -ave ssh mjirik@147.228.240.61:/mnt/proNAS/public/korpusy/paper_graphics/ ~/projects/paper_graphics/
+rsync -O -ave ssh mjirik@147.228.240.61:/mnt/proNAS/korpusy/paper_graphics/ ~/projects/paper_graphics/
 # rsync -ave ssh --delete ~/projects/paper_graphics/ mjirik@147.228.240.61:/mnt/proNAS/public/queetech/paper_graphics/
 
 
